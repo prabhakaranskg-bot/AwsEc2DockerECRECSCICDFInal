e6a7095f9c6616b5af19a525085acba221ae9e16
Prabhakaran G <prabhakaransaai@gmail.com>
2025-12-12 20:53:12 +0530

    first commit

---
+++ src/main/java/com/aws/ecr/ecs/AwsEc2DockerEcrecscicdfInalApplication.java
@@ -0,0 +1,13 @@
+package com.aws.ecr.ecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEc2DockerEcrecscicdfInalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEc2DockerEcrecscicdfInalApplication.class, args);
+	}
+
+}
